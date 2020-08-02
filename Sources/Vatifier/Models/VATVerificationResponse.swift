58fd4151cdd181e7db05eb5e2192f44b7b5b3fca
Mads Odgaard <mads@madsodgaard.com>
2020-08-02 20:05:58 +0200

    Add public initializer to response struct

--- Sources/Vatifier/Models/VATVerificationResponse.swift
+++ Sources/Vatifier/Models/VATVerificationResponse.swift
@@ -4,4 +4,10 @@ public struct VATVerificationResponse: Content {
     public let isValid: Bool
     public let name: String?
     public let address: String?
+    
+    public init(isValid: Bool, name: String? = nil, address: String? = nil) {
+        self.isValid = isValid
+        self.name = name
+        self.address = address
+    }
 }
