0cabe5b0c9d64b7dd04657be403342e71667dd85
JeronimoPaganini <shabonov@gmail.com>
2022-04-15 08:31:55 +0200

    Fixing accessebility

--- Sources/Vatifier/Client+Async.swift
+++ Sources/Vatifier/Client+Async.swift
@@ -1,7 +1,7 @@
 import Vapor
 
 extension VatifierClient {
-    func verify(_ vatNumber: String, country: Country) async throws -> VATVerificationResponse {
+    public func verify(_ vatNumber: String, country: Country) async throws -> VATVerificationResponse {
         try await verify(vatNumber, country: country).get()
     }
 }
